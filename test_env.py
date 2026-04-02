bc4aff9a83e916a2dc8fbc167197f8974ff24e03
zakariyyabrewster <zakariyya@sprung-brett.com>
2026-04-02 08:46:35 -0400

    requirements

--- test_env.py
+++ test_env.py
@@ -14,9 +14,9 @@ from backend.bazaar import Bazaar, BasicBazaar
 
 # from agents.mansa2_agent import MansaAgent as Agent1
 # from agents.mansa_agent import MansaAgent as Agent1
-from agents.optimal_agent import OptimalAgent as Agent1
-from agents.simple_agent import SmartAgent as Agent2
-# from agents.random_agent import RandomAgent as Agent2
+# from agents.optimal_agent import OptimalAgent as Agent1
+from agents.simple_agent import SmartAgent as Agent1
+from agents.random_agent import RandomAgent as Agent2
 
 NUM_GAMES = 100    # How many games to play total
 SEED_START = 100       # Starting random seed (change for different matchups)
